896976d9d0c69c47d5f5f2bd404238ac1bdf7d37
Stephanie Weirich <sweirich@cis.upenn.edu>
2015-05-21 14:17:54 -0400

    edits after FC call

--- hs/singletons.hs
+++ hs/singletons.hs
@@ -17,21 +17,34 @@ import Data.Proxy
 import GHC.TypeLits
 import Unsafe.Coerce
 
+----------------------------------------------------------------------
 -- basic singletons stuff
+----------------------------------------------------------------------
 
 data family Sing (a :: k)
+
+class SingI (a :: k) where sing :: Sing a
 
 class SingKind k where
   type DemoteRep k :: *
   fromSing :: Sing (x :: k) -> DemoteRep k
 
--- singletons and type-lit natural numbers 
 
+----------------------------------------------------------------------
+-- singletons for GHC.TypeLit Nats
+----------------------------------------------------------------------
+    
 data instance Sing (n :: Nat) = KnownNat n => SNat
+
+instance KnownNat n => SingI n where sing = SNat
 
 instance SingKind Nat where
   type DemoteRep Nat = Integer
   fromSing (SNat :: Sing n) = natVal (Proxy :: Proxy n)
+
+  
+sEqNat :: forall (a :: Nat) b. Sing a -> Sing b -> Maybe (a :~: b)
+sEqNat SNat SNat = sameNat (Proxy :: Proxy a) (Proxy :: Proxy b)
   
 
 (%:+) :: forall a b . Sing a -> Sing b -> Sing (a + b)
@@ -44,8 +57,10 @@ sa %:+ sb =
       Just (SomeNat (_ :: Proxy ab)) -> unsafeCoerce (SNat :: Sing ab)
       Nothing                        -> error "Two naturals added to a negative?"
 
-
+----------------------------------------------------------------------
 -- singletons for lists and booleans
+----------------------------------------------------------------------
+      
 data instance Sing (a :: [k]) where
   SNil  :: Sing '[]
   SCons :: Sing h -> Sing t -> Sing (h ': t)
@@ -55,7 +70,7 @@ data instance Sing (a :: Bool) where
   SFalse :: Sing 'False
   STrue :: Sing 'True
 
--- If type family from Data.Type.Bool
+-- `If` type family from Data.Type.Bool
 sIf :: Sing a -> Sing b -> Sing c -> Sing (If a b c)
 sIf SFalse _ c = c
 sIf STrue  b _ = b
--- hs/wg28.hs
+++ hs/wg28.hs
@@ -2,7 +2,7 @@
              DataKinds, TypeFamilies, UndecidableInstances,
              FlexibleContexts, RankNTypes, ScopedTypeVariables,
              FlexibleInstances #-}
-{-# OPTIONS_GHC -fwarn-unticked-promoted-constructors -fprint-explicit-kinds #-}
+{-# OPTIONS_GHC -fwarn-unticked-promoted-constructors -Wall -fprint-explicit-kinds #-}
 
 
 module Wg28 where
@@ -11,6 +11,7 @@ module Wg28 where
 
    Towards Dependently-typed Haskell 
 
+   Stephanie Weirich, University of Pennsylvania
    joint work with Richard Eisenberg
 
    this code requires Richard's fork of GHC
@@ -21,29 +22,33 @@ module Wg28 where
 -}
 
 
-import Data.Type.Equality 
-import GHC.Exts
-import Data.Type.Bool
-import Data.Proxy
+import Data.Type.Equality ((:~:)(..))
+import Data.Type.Bool (If)
 import GHC.TypeLits
-import Singletons (Sing(..), (%:+), sIf)
+
+import Singletons (Sing(..), (%:+), sIf, SingI(..), sEqNat)
+
+-- ROAD MAP:
+--    
+
 
 {- Part I: Dynamic Typing in a Statically Typed Language -}
 
 -- Old and (somewhat) boring
 
+{-
 data Dynamic where
-   Dyn :: Typerep a -> a -> Dynamic
+   Dyn :: TypeRep a -> a -> Dynamic
 
-data Typerep (a :: *) where
-  TBool  :: Typerep Bool
-  TFun   :: Typerep t1 -> Typerep t2 -> Typerep (t1 -> t2)
-  TProd  :: Typerep t1 -> Typerep t2 -> Typerep (t1, t2)
+data TypeRep (a :: *) where
+  TBool  :: TypeRep Bool
+  TFun   :: TypeRep t1 -> TypeRep t2 -> TypeRep (t1 -> t2)
+  TProd  :: TypeRep t1 -> TypeRep t2 -> TypeRep (t1, t2)
   -- let's get fancy!
-  TDynamic :: Typerep Dynamic
-  TTyperep :: Typerep a -> Typerep (Typerep a)
-  -- Note: can't do ReaderT r m a 
-  -- TReaderT :: Typerep r -> Typerep m -> Typerep a -> Typerep (ReaderT r m a)
+  TDynamic :: TypeRep Dynamic
+  TTypeRep :: TypeRep a -> TypeRep (TypeRep a)
+  -- Note: can't do MaybeT m a 
+  -- TMaybeT :: TypeRep m -> TypeRep a -> TypeRep (MaybeT m a)
 
 dynIf :: Dynamic -> a -> a -> a
 dynIf (Dyn TBool True) t _   = t
@@ -52,42 +57,43 @@ dynIf (Dyn TDynamic d) t f   = dynIf d t f
 dynIf (Dyn _ _) _ _ = error "runtime type error"
 
 dynApply :: Dynamic -> Dynamic -> Dynamic
-dynApply (Dyn (TFun t1 t2) f) (Dyn t3 x) = case eqtype t1 t3 of
+dynApply (Dyn (TFun t1 t2) f) (Dyn t3 x) = case eqT t1 t3 of
   Just Refl -> Dyn t2 (f x)
+  Nothing   -> error "runtime type error"
 dynApply (Dyn TDynamic d1) d2 = dynApply d1 d2
 dynApply (Dyn _ _) _ = error "runtime type error"
 
 dynFst :: Dynamic -> Dynamic
-dynFst (Dyn (TProd t1 t2) (x1,_)) = Dyn t1 x1
-dynFst (Dyn TDynamic d1) = dynFst d1 
+dynFst (Dyn (TProd t1 _) (x1,_)) = Dyn t1 x1
+dynFst (Dyn TDynamic d1) = dynFst d1
 dynFst (Dyn _ _) = error "runtime type error"
 
-eqtype :: Typerep a -> Typerep b -> Maybe (a :~: b)
-eqtype TBool TBool = Just Refl
-eqtype (TFun a1 b1) (TFun a2 b2) = case eqtype a1 a2 of 
-  Just Refl -> case eqtype b1 b2 of
+eqT :: TypeRep a -> TypeRep b -> Maybe (a :~: b)
+eqT TBool TBool = Just Refl
+eqT (TFun a1 b1) (TFun a2 b2) = case eqT a1 a2 of 
+  Just Refl -> case eqT b1 b2 of
      Just Refl -> Just Refl
      Nothing -> Nothing
   Nothing -> Nothing
-eqtype (TProd a1 b1) (TProd a2 b2) = case eqtype a1 a2 of 
-  Just Refl -> case eqtype b1 b2 of
+eqT (TProd a1 b1) (TProd a2 b2) = case eqT a1 a2 of 
+  Just Refl -> case eqT b1 b2 of
      Just Refl -> Just Refl
      Nothing -> Nothing
   Nothing -> Nothing
-eqtype TDynamic TDynamic = Just Refl
-eqtype (TTyperep a1) (TTyperep a2) = case eqtype a1 a2 of 
+eqT TDynamic TDynamic = Just Refl
+eqT (TTypeRep a1) (TTypeRep a2) = case eqT a1 a2 of 
   Just Refl -> Just Refl
   Nothing -> Nothing
-eqtype _ _ = Nothing
+eqT _ _ = Nothing
 
-
+-}
 
 
 -- New! Improved!  And quite crazy.
 
 -- Watch for: kind-indexed GADT,  star :: star
 
-{-
+
 data Tycon (a :: k) where
   TBool    :: Tycon Bool
   TFun     :: Tycon (->)
@@ -95,15 +101,15 @@ data Tycon (a :: k) where
   TMaybe   :: Tycon Maybe
   TDynamic :: Tycon Dynamic
   -- Note: typerep is now kind-polymorphic. We'll represent the instances
-  TTyperep :: Typerep k -> Tycon (Typerep :: k -> *)
+  TTypeRep :: TypeRep k -> Tycon (TypeRep :: k -> *)
   TStar    :: Tycon *
   
-data Typerep (a :: k) where
-  TCon  :: Tycon c -> Typerep c
-  TApp  :: Typerep a -> Typerep b -> Typerep (a b)
+data TypeRep (a :: k) where
+  TCon  :: Tycon c -> TypeRep c
+  TApp  :: TypeRep a -> TypeRep b -> TypeRep (a b)
 
 d1 :: Dynamic
-d1 = Dyn (TApp (TCon (TTyperep (TCon TStar))) (TCon TBool)) (TCon TBool)
+d1 = Dyn (TApp (TCon (TTypeRep (TCon TStar))) (TCon TBool)) (TCon TBool)
 
 
 dynIf :: Dynamic -> a -> a -> a
@@ -112,16 +118,16 @@ dynIf (Dyn (TCon TBool) False) _ f = f
 dynIf (Dyn _ _) _ _ = error "runtime type error"
 
 dynApply :: Dynamic -> Dynamic -> Dynamic
-dynApply (Dyn (TApp (TApp (TCon TFun) t1) t2) f) (Dyn t3 x) = case eqtype t1 t3 of
-  Just HRefl -> Dyn t2 (f x)
+dynApply (Dyn (TApp (TApp (TCon TFun) t1) t2) f) (Dyn t3 x)
+  | Just HRefl <- eqT t1 t3 = Dyn t2 (f x)
 dynApply (Dyn _ _) _ = error "runtime type error"
 
 dynFst :: Dynamic -> Dynamic
-dynFst (Dyn (TApp (TApp (TCon TProd) t1) t2) (x1,_)) = Dyn t1 x1
+dynFst (Dyn (TApp (TApp (TCon TProd) t1) _) (x1,_)) = Dyn t1 x1
 dynFst (Dyn _ _) = error "runtime type error"
 
 data Dynamic where
-   Dyn :: Typerep a -> a -> Dynamic
+   Dyn :: TypeRep a -> a -> Dynamic
 
 
 data ((a :: k1) :~~: (b :: k2)) where
@@ -133,21 +139,21 @@ eqtycon TFun  TFun        = Just HRefl
 eqtycon TProd TProd       = Just HRefl
 eqtycon TMaybe TMaybe     = Just HRefl
 eqtycon TDynamic TDynamic = Just HRefl
-eqtycon (TTyperep k1) (TTyperep k2) = case eqtype k1 k2 of
+eqtycon (TTypeRep k1) (TTypeRep k2) = case eqT k1 k2 of
   Just HRefl -> Just HRefl
   Nothing    -> Nothing 
 eqtycon TStar TStar       = Just HRefl  
 eqtycon _ _ = Nothing
 
-eqtype :: Typerep a -> Typerep b -> Maybe (a :~~: b)
-eqtype (TCon c1) (TCon c2) = eqtycon c1 c2
-eqtype (TApp a1 b1) (TApp a2 b2) = case eqtype a1 a2 of 
-  Just HRefl -> case eqtype b1 b2 of
+eqT :: TypeRep a -> TypeRep b -> Maybe (a :~~: b)
+eqT (TCon c1) (TCon c2) = eqtycon c1 c2
+eqT (TApp a1 b1) (TApp a2 b2) = case eqT a1 a2 of 
+  Just HRefl -> case eqT b1 b2 of
      Just HRefl -> Just HRefl
      Nothing -> Nothing
   Nothing -> Nothing
-eqtype _ _ = Nothing
--}
+eqT _ _ = Nothing
+
 
 {- Part II: DTH-curious? -}
 
@@ -168,8 +174,8 @@ eval (Cond e0 e1 e2) = if eval e0 then eval e1 else eval e2
 -- constant folding function. GADT tells us that it preserves types.
 optimize :: (Eq n, Num n) => Expr n t -> Expr n t
 optimize (Val x) = Val x
-optimize (Cond (Val True) e1 e2) = optimize e1
-optimize (Cond (Val False) e1 e2) = optimize e2
+optimize (Cond (Val True) e1 _) = optimize e1
+optimize (Cond (Val False) _ e2) = optimize e2
 optimize (Cond e1 e2 e3) = Cond (optimize e1) (optimize e2) (optimize e3)
 optimize (Plus (Val 0) e) = optimize e
 optimize (Plus e (Val 0)) = optimize e
@@ -186,7 +192,8 @@ type family Eval (x :: Expr Nat t) :: t where
 -- a Singleton for that promoted GADT (i.e. a GADT indexed by a GADT)
 -- Sing is a data family for singleton types
         
--- NOTE that GHC.TypeLits uses Integers as the runtime representation of type-level Nats        
+-- NOTE that GHC.TypeLits uses Integers as the runtime representation
+-- of type-level Nats        
 data instance Sing (e :: Expr Nat t) where
   SVal  :: Sing (u :: t) -> Sing ('Val u :: Expr Nat t)
   SPlus :: Sing a -> Sing b -> Sing ('Plus a b :: Expr Nat Nat)
@@ -198,6 +205,8 @@ sEval (SVal n) = n
 sEval (e1 `SPlus` e2) = (sEval e1) %:+ (sEval e2)
 sEval (SCond e0 e1 e2) = sIf (sEval e0) (sEval e1) (sEval e2)                                                 
 
+
+---------------------------------------------------------
 -- The optimizer should produce an equivalent expression
 
 data Equivalent e where
@@ -206,21 +215,21 @@ data Equivalent e where
 sOpt :: Sing e -> Equivalent e
 sOpt (SVal x) = Result (SVal x) 
 
-sOpt (SCond (SVal STrue)  e1 e2) = Result e1
-sOpt (SCond (SVal SFalse) e1 e2) = Result e2
+sOpt (SCond (SVal STrue)  e1 _) = Result e1
+sOpt (SCond (SVal SFalse) _ e2) = Result e2
 sOpt (SCond e0 e1 e2) = case (sOpt e0, sOpt e1, sOpt e2) of
   (Result e0', Result e1', Result e2') -> Result (SCond e0' e1' e2')
 
 -- uses TypeNat equality (0 + n) ~ n
-sOpt e@(SPlus (SVal (SNat :: Sing n0)) se) =
-  case sameNat (Proxy :: Proxy n0) (Proxy :: Proxy 0) of
+sOpt e@(SPlus (SVal sn) se) =
+  case sEqNat sn (sing :: Sing 0) of
      Just Refl -> case sOpt se of
        Result se' -> Result se' 
      Nothing   -> Result e
      
 -- uses TypeNat equality (n + 0) ~ n
-sOpt e@(SPlus se (SVal (SNat :: Sing n0))) =
-  case sameNat (Proxy :: Proxy n0) (Proxy :: Proxy 0) of
+sOpt e@(SPlus se (SVal sn)) =
+  case sEqNat sn (sing :: Sing 0) of
      Just Refl -> case sOpt se of
        Result se' -> Result se'
      Nothing   -> Result e
@@ -242,7 +251,7 @@ sOpt (SPlus e0 e1) = case (sOpt e0, sOpt e1) of
 
 {-
 data Equivalent e where
-  Result :: pi e -> (Eval e ~ Eval e') => Equivalent e
+  Result :: pi e' -> (Eval e ~ Eval e') => Equivalent e
 
 opt :: pi e -> Equivalent e
 opt (Val x) = Result (Val x) 
