a109e1cf1c001d2d6568eef6bb001d6e0b10270e
Stephanie Weirich <sweirich@cis.upenn.edu>
2015-05-26 09:42:52 +0300

    version of evaluator with variables, prompted by question from Robby

--- hs/robby.hs
+++ hs/robby.hs
@@ -146,11 +146,6 @@ dynFst (Dyn (TApp (TApp (TCon TProd) t1) _) (x1,_)) = Dyn t1 x1
 dynFst (Dyn _ _) = error "runtime type error"
 
 
-{-
--- Defined in Data.Type.Equality
-data ((a :: k) :~: (b :: k)) where
-  Refl :: (a ~ b) => a :~: b
--}
 
 data ((a :: k1) :~~: (b :: k2)) where
   HRefl :: (k1 ~ k2, a ~ b) => a :~~: b  
@@ -179,18 +174,37 @@ eqT _ _ = Nothing
 
 -- first parameter is the type for numbers, second is the type of the
 -- entire expression
-data Expr (n :: *) :: * -> * where
-  Val  :: t -> Expr n t
-  Plus :: Expr n n -> Expr n n -> Expr n n 
-  Cond :: Expr n Bool -> Expr n t -> Expr n t -> Expr n t
-  
-eval :: Num n => Expr n t -> t
-eval (Val n)         = n
-eval (e1 `Plus` e2)  = eval e1 + eval e2
-eval (Cond e0 e1 e2) = if eval e0 then eval e1 else eval e2
+data Expr (g :: [*]) (n :: *) :: * -> * where
+  Val  :: t -> Expr g n t
+  Plus :: Expr g n n -> Expr g n n -> Expr g n n 
+  Cond :: Expr g n Bool -> Expr g n t -> Expr g n t -> Expr g n t
+  Var  :: Var g t -> Expr g n t
+
+data Var :: [*] -> * -> * where
+  VZ   :: Var (t ': ts) t
+  VS   :: Var ts u -> Var (t ': ts) u
+
+
+data Env ( g :: [*] ) :: * where
+  EZ   :: Env '[]
+  ES   :: t -> Env ts -> Env (t ': ts)
+
+                         
+vlookup :: Var g t -> Env g -> t
+vlookup VZ (ES x _) = x
+vlookup (VS v) (ES _ xs) = vlookup v xs
+
+
+                          
+eval :: Num n =>  Expr g n t -> Env g -> t
+eval (Val n)         r = n
+eval (e1 `Plus` e2)  r = eval e1 r + eval e2 r
+eval (Cond e0 e1 e2) r = if eval e0 r then eval e1 r else eval e2 r
+eval (Var x)         r = vlookup x r
+
 
 -- constant folding function. GADT tells us that it preserves types.
-optimize :: (Eq n, Num n) => Expr n t -> Expr n t
+optimize :: (Eq n, Num n) => Expr g n t -> Expr g n t
 optimize (Val x) = Val x
 optimize (Cond (Val True) e1 _) = optimize e1
 optimize (Cond (Val False) _ e2) = optimize e2
@@ -198,68 +212,104 @@ optimize (Cond e1 e2 e3) = Cond (optimize e1) (optimize e2) (optimize e3)
 optimize (Plus (Val 0) e) = optimize e
 optimize (Plus e (Val 0)) = optimize e
 optimize (Plus e1 e2) = Plus (optimize e1) (optimize e2)
+optimize (Var x) = Var x
+
 
 -- New! Type family for a promoted GADT
 
 -- We promote this datatype using GHC.TypeLit's "Nat" kind for type-level
 -- numbers
 
-type family Eval (x :: Expr Nat t) :: t where
-  Eval ('Val n) = n
-  Eval ('Plus e1 e2) = Eval e1 + Eval e2
-  Eval ('Cond e0 e1 e2) = If (Eval e0) (Eval e1) (Eval e2)
+type family VLookup (v :: Var g t) (e :: Env g) :: t where
+  VLookup 'VZ ('ES x xs) = x
+  VLookup ('VS v) ('ES x xs) = VLookup v xs
+
+type family Eval (x :: Expr g Nat t) (r :: Env g) :: t where
+  Eval ('Val n) r = n
+  Eval ('Plus e1 e2) r = Eval e1 r + Eval e2 r
+  Eval ('Cond e0 e1 e2) r = If (Eval e0 r) (Eval e1 r) (Eval e2 r)
+  Eval ('Var x) r = VLookup x r
+  -- Eval ('Abs t) r = \x -> Eval t (x ': r)
+  
 
 -- a Singleton for that promoted GADT (i.e. a GADT indexed by a GADT)
 -- Sing is a data family for singleton types
         
 -- NOTE that we have defined (Sing u :: Nat) to use Integers as the
 -- runtime representation of type-level Nats (see singletons.hs)
-        
-data instance Sing (e :: Expr Nat t) where
-  SVal  :: Sing (u :: t) -> Sing ('Val u :: Expr Nat t)
-  SPlus :: Sing a -> Sing b -> Sing ('Plus a b :: Expr Nat Nat)
-  SCond :: Sing a -> Sing b -> Sing c -> Sing ('Cond a b c :: Expr Nat t)
 
+
+data instance Sing (e :: Expr g Nat t) where
+  SVal  :: Sing (u :: t) -> Sing ('Val u :: Expr g Nat t)
+  SPlus :: Sing a -> Sing b -> Sing ('Plus a b :: Expr g Nat Nat)
+  SCond :: Sing a -> Sing b -> Sing c -> Sing ('Cond a b c :: Expr g Nat t)
+  SVar  :: Sing v -> Sing ('Var v :: Expr g Nat t)
+
+data instance Sing (v :: Var g t) where
+  SVZ   :: Sing ('VZ :: Var (t ': ts) t)
+  SVS   :: Sing (vs  :: Var ts u) -> Sing ('VS vs :: Var (t ': ts) u)
+
+data instance Sing (r :: Env g) where
+  SEZ   :: Sing ('EZ :: Env '[])
+  SES   :: Sing (u :: t) -> Sing (us :: Env ts)
+            -> Sing (ES u us :: Env (t ': ts))
+                                                                      
+
+sLookup :: Sing (v :: Var g t) -> Sing (r :: Env g) -> Sing (VLookup v r)
+sLookup SVZ (SES sx _) = sx
+sLookup (SVS sv) (SES _ sxs) = sLookup sv sxs
+
+
+                                     
 -- An evaluator for the singletons; in lock-step with the type level
-sEval :: Sing e -> Sing (Eval e)
-sEval (SVal n) = n
-sEval (e1 `SPlus` e2) = (sEval e1) %:+ (sEval e2)
-sEval (SCond e0 e1 e2) = sIf (sEval e0) (sEval e1) (sEval e2)                                                 
+sEval :: Sing e -> Sing g -> Sing (Eval e g)
+sEval (SVal n) r = n
+sEval (e1 `SPlus` e2) r = (sEval e1 r) %:+ (sEval e2 r)
+sEval (SCond e0 e1 e2) r = sIf (sEval e0 r) (sEval e1 r) (sEval e2 r)
+sEval (SVar x) r = sLookup x r
 
 
 ---------------------------------------------------------
 -- The optimizer should produce an equivalent expression
 
 data Equivalent e where
-  Result :: (Eval e ~ Eval e') => Sing e' -> Equivalent e
+  -- Result :: (forall r. Eval e r ~ Eval e' r) => Sing e' -> Equivalent e
+  Result :: (forall r. Sing r -> Eval e r :~: Eval e' r) -> Sing e' -> Equivalent e
 
 
 sOpt :: Sing e -> Equivalent e
-sOpt (SVal x) = Result (SVal x) 
+sOpt (SVal x) = Result (\ _ -> Refl) (SVal x) 
 
-sOpt (SCond (SVal STrue)  e1 _) = Result e1
-sOpt (SCond (SVal SFalse) _ e2) = Result e2
+sOpt (SCond (SVal STrue)  e1 _) = Result (\ _ -> Refl) e1
+sOpt (SCond (SVal SFalse) _ e2) = Result (\ _ -> Refl) e2
 sOpt (SCond e0 e1 e2) = case (sOpt e0, sOpt e1, sOpt e2) of
-  (Result e0', Result e1', Result e2') -> Result (SCond e0' e1' e2')
+  (Result p0 e0', Result p1 e1', Result p2 e2') ->
+    Result (\ r -> case (p0 r, p1 r, p2 r) of
+                    (Refl, Refl, Refl) -> Refl) (SCond e0' e1' e2')
 
 -- uses TypeNat equality (0 + n) ~ n
 sOpt e@(SPlus (SVal sn) se) =
   case sEqNat sn (sing :: Sing 0) of
      Just Refl -> case sOpt se of
-       Result se' -> Result se' 
-     Nothing   -> Result e
-     
+       Result p se' -> Result (\r -> case p r of
+                                      Refl -> Refl) se' 
+     Nothing   -> Result (\_ -> Refl) e
+
 -- uses TypeNat equality (n + 0) ~ n
 sOpt e@(SPlus se (SVal sn)) =
   case sEqNat sn (sing :: Sing 0) of
      Just Refl -> case sOpt se of
-       Result se' -> Result se'
-     Nothing   -> Result e
+       Result p se' -> Result (\r -> case p r of
+                                      Refl -> Refl) se'
+     Nothing   -> Result (\_ -> Refl) e
+
 
 sOpt (SPlus e0 e1) = case (sOpt e0, sOpt e1) of
-   (Result e0', Result e1') -> Result (SPlus e0' e1')
+   (Result p0 e0', Result p1 e1') ->
+     Result (\r -> case (p0 r, p1 r) of
+                    (Refl, Refl) -> Refl) (SPlus e0' e1')
 
-
+sOpt (SVar x) = Result (\_ -> Refl) (SVar x)
 
 
 
